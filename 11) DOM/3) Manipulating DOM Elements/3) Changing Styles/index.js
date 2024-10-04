7aa72cd9b596f22f040d8af9750e78ee448a0688
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-10-04 21:20:59 +0500

    first commit

--- 11) DOM/3) Manipulating DOM Elements/3) Changing Styles/index.js
+++ 11) DOM/3) Manipulating DOM Elements/3) Changing Styles/index.js
@@ -5,7 +5,12 @@ const div = document.querySelector('div');
 
 //? When using the style property, CSS property names are converted to camelCase. For example, background-color becomes backgroundColor
 para.style.fontFamily = "Segoe Ui"
-para.style.fontSize = "200px"
+para.style.fontSize = "20px"
+para.style.background = "red"
 //? he style property only applies to inline styles. It does not include styles defined in external stylesheets or <style> tags in the <head> section
 
 //! Javascript styles have the highest priority
+div.style.height='100px'
+div.style.width='100px'
+//? uncomment code below
+// div.style.background='lime'
