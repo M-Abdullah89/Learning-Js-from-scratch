8e34beb9c304fbde4c1ce19040e762021a2d34dc
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-10-04 15:08:45 +0500

    first commit

---
+++ 11) DOM/3) Manipulating DOM Elements/3) Changing Styles/index.js
@@ -0,0 +1,9 @@
+//! The style property in JavaScript DOM is used to get or set the inline style of an HTML element.
+
+
+
+//? When using the style property, CSS property names are converted to camelCase. For example, background-color becomes backgroundColor
+
+//? he style property only applies to inline styles. It does not include styles defined in external stylesheets or <style> tags in the <head> section
+
+//! Javascript styles have the highest priority
