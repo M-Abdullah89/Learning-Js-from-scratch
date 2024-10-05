d7a49634145484fe3131c132c48ef62ecb07846e
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-10-05 08:09:40 +0500

    first commit

---
+++ 11) DOM/4) Creating and Removing Elements/1) Creating Elements/index.js
@@ -0,0 +1,9 @@
+//! The document.createElement() method in JavaScript is used to create a new HTML element specified by the tag name you provide
+
+//* document.createElement(tagName);
+
+const Div = document.createElement("div");
+newDiv.textContent = "Hello, World!";
+
+//! it will not show as it is not inserted in the document
+//? for that we will use append child
---
+++ 11) DOM/4) Creating and Removing Elements/2) Appending Elements/appendChild Method/index.js
@@ -0,0 +1,15 @@
+//! the appendChild() method is used to add a new child node to the end of the list of children of a specified parent node.
+//?  This method is commonly used in DOM manipulation to dynamically add new elements to an existing HTML structure
+
+//! This method is not safe
+const newParagraph1 = document.createElement("p");
+newParagraph1.innerText="Hello"
+
+document.body.appendChild(newParagraph1);
+
+//! This is safe method
+const newParagraph2 = document.createElement("p");
+const textContent = document.createTextNode("This is a new paragraph.");
+newParagraph2.appendChild(textContent);
+document.body.appendChild(newParagraph2);
+    
