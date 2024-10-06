7de472e5749e11afe44209478f55345d732af8ab
M-Abdullah89 <166618916+M-Abdullah89@users.noreply.github.com>
2024-10-06 11:39:57 +0500

    first commit

---
+++ 12) Advanced DOM Manipulation/3) Template Literals/index.js
@@ -0,0 +1,25 @@
+//! Template literals are strings enclosed by backticks (`) that allow for embedded expressions and multi-line strings. They are particularly useful for generating HTML dynamically.
+
+//?  When generating HTML dynamically, you often need to insert variables and expressions into your HTML structure. Template literals make this process straightforward and readable.
+
+
+
+const user = {
+    name: 'Alice',
+    age: 30,
+    city: 'Wonderland'
+};
+
+const userCard = `
+    <div class="user-card">
+        <h2>${user.name}</h2>
+        <p>Age: ${user.age}</p>
+        <p>City: ${user.city}</p>
+    </div>
+`;
+
+document.body.innerHTML = userCard;
+
+
+//? it is useful for Displaying user profiles, lists, or any dynamic content fetched from an API.
+//? Creating reusable HTML templates for components in frameworks like React or Vue.
